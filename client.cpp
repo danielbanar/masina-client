2c0b5419ac2669a8bd5718b263b0624d85338e8c
Daniel Banar <daniel.banar5@gmail.com>
2025-10-21 22:55:42 +0200

    failsafe fix

--- client.cpp
+++ client.cpp
@@ -186,7 +186,7 @@ int main()
     while (true)
     {
         usleep(1000);
-        static uint16_t channels[16] = {992, 992, 1716, 992, 191, 191, 191, 191, 997, 997, 997, 997, 0, 0, 1811, 1811};
+        static uint16_t crsfChannels[16] = {992, 992, 1716, 992, 191, 191, 191, 191, 997, 997, 997, 997, 0, 0, 1811, 1811};
         static bool fsMode = false;
         static auto lastValidPayload = std::chrono::high_resolution_clock::now();
         static auto lastSentPayload = std::chrono::high_resolution_clock::now();
@@ -240,19 +240,19 @@ int main()
                     {
                         // No data for 5s - Failsafe
                         // std::cerr << "FAILSAFE_TIMEOUT\n";
-                        channels[11] = fsMode ? CRSF_CHANNEL_VALUE_2000 : CRSF_CHANNEL_VALUE_MID;
+                        crsfChannels[10] = fsMode ? US_TO_CRSF(2000) : US_TO_CRSF(1500);
                         if (gConfig.debug)
-                            printf("FS TIMEOUT!\n");
+                            printf("FS TIMEOUT! - %s", fsMode ? "LAND\n" : "GPS\n");
                     }
                     else if (elapsedTimeValid >= gConfig.stabTimeout)
                     {
                         // No data for 250ms - STABILIZE
                         // std::cerr << "STABILIZE_TIMEOUT\n";
-                        channels[0] = CRSF_CHANNEL_VALUE_MID;         // ROLL
-                        channels[1] = CRSF_CHANNEL_VALUE_MID;         // PITCH
-                        channels[2] = US_TO_CRSF(gConfig.hoverValue); // THROTTLE
-                        channels[3] = CRSF_CHANNEL_VALUE_MID;         // YAW
-                        channels[5] = CRSF_CHANNEL_VALUE_MIN;         // ANGLE MODE MODE
+                        crsfChannels[0] = CRSF_CHANNEL_VALUE_MID;         // ROLL
+                        crsfChannels[1] = CRSF_CHANNEL_VALUE_MID;         // PITCH
+                        crsfChannels[2] = US_TO_CRSF(gConfig.hoverValue); // THROTTLE
+                        crsfChannels[3] = CRSF_CHANNEL_VALUE_MID;         // YAW
+                        crsfChannels[5] = CRSF_CHANNEL_VALUE_MIN;         // ANGLE MODE MODE
                         if (gConfig.debug)
                             printf("STAB TIMEOUT!\n");
                     }
@@ -263,28 +263,28 @@ int main()
                         crsfPayload[0] = 0xC8;
                         crsfPayload[1] = 0x18;
                         crsfPayload[2] = 0x16;
-                        crsfPayload[3] = (channels[0] & 0xFF);
-                        crsfPayload[4] = ((channels[0] >> 8) & 0x07) | ((channels[1] & 0x1F) << 3);
-                        crsfPayload[5] = ((channels[1] >> 5) & 0x3F) | ((channels[2] & 0x03) << 6);
-                        crsfPayload[6] = ((channels[2] >> 2) & 0xFF);
-                        crsfPayload[7] = ((channels[2] >> 10) & 0x01) | ((channels[3] & 0x7F) << 1);
-                        crsfPayload[8] = ((channels[3] >> 7) & 0x0F) | ((channels[4] & 0x0F) << 4);
-                        crsfPayload[9] = ((channels[4] >> 4) & 0x7F) | ((channels[5] & 0x01) << 7);
-                        crsfPayload[10] = ((channels[5] >> 1) & 0xFF);
-                        crsfPayload[11] = ((channels[5] >> 9) & 0x03) | ((channels[6] & 0x3F) << 2);
-                        crsfPayload[12] = ((channels[6] >> 6) & 0x1F) | ((channels[7] & 0x07) << 5);
-                        crsfPayload[13] = ((channels[7] >> 3) & 0xFF);
-                        crsfPayload[14] = (channels[8] & 0xFF);
-                        crsfPayload[15] = ((channels[8] >> 8) & 0x07) | ((channels[9] & 0x1F) << 3);
-                        crsfPayload[16] = ((channels[9] >> 5) & 0x3F) | ((channels[10] & 0x03) << 6);
-                        crsfPayload[17] = ((channels[10] >> 2) & 0xFF);
-                        crsfPayload[18] = ((channels[10] >> 10) & 0x01) | ((channels[11] & 0x7F) << 1);
-                        crsfPayload[19] = ((channels[11] >> 7) & 0x0F) | ((channels[12] & 0x0F) << 4);
-                        crsfPayload[20] = ((channels[12] >> 4) & 0x7F) | ((channels[13] & 0x01) << 7);
-                        crsfPayload[21] = ((channels[13] >> 1) & 0xFF);
-                        crsfPayload[22] = ((channels[13] >> 9) & 0x03) | ((channels[14] & 0x3F) << 2);
-                        crsfPayload[23] = ((channels[14] >> 6) & 0x1F) | ((channels[15] & 0x07) << 5);
-                        crsfPayload[24] = ((channels[15] >> 3) & 0xFF);
+                        crsfPayload[3] = (crsfChannels[0] & 0xFF);
+                        crsfPayload[4] = ((crsfChannels[0] >> 8) & 0x07) | ((crsfChannels[1] & 0x1F) << 3);
+                        crsfPayload[5] = ((crsfChannels[1] >> 5) & 0x3F) | ((crsfChannels[2] & 0x03) << 6);
+                        crsfPayload[6] = ((crsfChannels[2] >> 2) & 0xFF);
+                        crsfPayload[7] = ((crsfChannels[2] >> 10) & 0x01) | ((crsfChannels[3] & 0x7F) << 1);
+                        crsfPayload[8] = ((crsfChannels[3] >> 7) & 0x0F) | ((crsfChannels[4] & 0x0F) << 4);
+                        crsfPayload[9] = ((crsfChannels[4] >> 4) & 0x7F) | ((crsfChannels[5] & 0x01) << 7);
+                        crsfPayload[10] = ((crsfChannels[5] >> 1) & 0xFF);
+                        crsfPayload[11] = ((crsfChannels[5] >> 9) & 0x03) | ((crsfChannels[6] & 0x3F) << 2);
+                        crsfPayload[12] = ((crsfChannels[6] >> 6) & 0x1F) | ((crsfChannels[7] & 0x07) << 5);
+                        crsfPayload[13] = ((crsfChannels[7] >> 3) & 0xFF);
+                        crsfPayload[14] = (crsfChannels[8] & 0xFF);
+                        crsfPayload[15] = ((crsfChannels[8] >> 8) & 0x07) | ((crsfChannels[9] & 0x1F) << 3);
+                        crsfPayload[16] = ((crsfChannels[9] >> 5) & 0x3F) | ((crsfChannels[10] & 0x03) << 6);
+                        crsfPayload[17] = ((crsfChannels[10] >> 2) & 0xFF);
+                        crsfPayload[18] = ((crsfChannels[10] >> 10) & 0x01) | ((crsfChannels[11] & 0x7F) << 1);
+                        crsfPayload[19] = ((crsfChannels[11] >> 7) & 0x0F) | ((crsfChannels[12] & 0x0F) << 4);
+                        crsfPayload[20] = ((crsfChannels[12] >> 4) & 0x7F) | ((crsfChannels[13] & 0x01) << 7);
+                        crsfPayload[21] = ((crsfChannels[13] >> 1) & 0xFF);
+                        crsfPayload[22] = ((crsfChannels[13] >> 9) & 0x03) | ((crsfChannels[14] & 0x3F) << 2);
+                        crsfPayload[23] = ((crsfChannels[14] >> 6) & 0x1F) | ((crsfChannels[15] & 0x07) << 5);
+                        crsfPayload[24] = ((crsfChannels[15] >> 3) & 0xFF);
                         crsfPayload[25] = CRC8(crsfPayload, 2, 0x18 - 1);
                         ssize_t bytes_written = write(serialPort, crsfPayload, 26);
                         /*static uint8_t linkPayload[15] = "\xC8\x0C\x14\x10\x17\x64\x05\x00\x01\x01\x00\x00\x00\x59"; //Dummy data
@@ -334,10 +334,9 @@ int main()
                         continue;
                     }
 
-                    /*printf("CHANNELS: ");
-                    for (size_t i = 0; i < payloadLength; i++)
+                    /*for (size_t i = 1; i < payloadLength; i++)
                     {
-                        printf("%5d",payload[i]);
+                        printf("CH%d %5d | ",i,payload[i]);
                     }
                     printf("\n");*/
 
@@ -349,9 +348,9 @@ int main()
                         lastN = N;
                         lastValidPayload = std::chrono::high_resolution_clock::now();
                         for (size_t i = 0; i < payloadLength - 1; i++)
-                            channels[i] = US_TO_CRSF(payload[i + 1]);
+                            crsfChannels[i] = US_TO_CRSF(payload[i + 1]);
 
-                        fsMode = matches[10] == 2000;
+                        fsMode = payload[10] == 2000;
                         bool remote = payload[9] == 2000;
                         static bool lastRemoteState = false;
                         if (remote != lastRemoteState)
@@ -361,10 +360,6 @@ int main()
                             std::system(command.c_str());
                             lastRemoteState = remote;
                         }
-
-                        /*for(int i = 0;i<10;i++)
-                        printf("%d ",payload[i]);
-                        printf("\n");*/
                     }
                     else
                     {
